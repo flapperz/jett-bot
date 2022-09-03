020695c24e36311de11817d7d4cfd70376f058ca
Krit Cholapand <flap.atyz@gmail.com>
2022-09-04 01:37:54 +0700

    Add ping and test command

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+from dis import dis
 import os
 from re import S
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+intents.message_content = True
 
-@client.event
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord')
+    print(f'{bot.user} has connected to Discord')
 
-client.run(DISCORD_TOKEN)
+@bot.command(name='ping')
+async def ping_command(ctx):
+    await ctx.send(f"Pong: Latency is {bot.latency * 1000:.3f}ms")
+
+@bot.command(name='echo')
+async def echo_command(ctx, arg):
+    await ctx.send(f"{arg}")
+
+bot.run(DISCORD_TOKEN)
 
